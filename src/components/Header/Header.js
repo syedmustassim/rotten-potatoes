5b533255ada737a5f573165b0ee124d193c87aea
Syed Mustassim <smustassim@gmail.com>
2023-07-26 22:17:43 +0530

    feat: header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1> Header </h1>
+    <div className="header">
+      <Link to="/">
+        <div className="logo">
+          <img
+            src="https://i.postimg.cc/k4cCFG5L/rotten-potatoes.png"
+            alt="logo"
+          />
+        </div>
+      </Link>
+      <div className="user-image">
+        <img src="https://i.postimg.cc/fTYff9yW/urahichi.gif" alt="user" />
+      </div>
     </div>
   );
 };
