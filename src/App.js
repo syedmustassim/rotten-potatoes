f11297d3931fddadcac5f3ce4c5b169267089761
Syed Mustassim <smustassim@gmail.com>
2023-07-26 21:42:54 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+
 import "./App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1> Hello World </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
