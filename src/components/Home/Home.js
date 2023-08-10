e6ff1227dc61834a87a30bb44b466ab6094948ae
Syed Mustassim <smustassim@gmail.com>
2023-08-10 22:08:27 +0530

    feat: API call in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import MovieListing from "../MovieListing/MovieListing";
+import MovieApi from "../../common/apis/MovieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
+  useEffect(() => {
+    const movieText = "Black";
+    const fetchMovies = async () => {
+      try {
+        const response = await MovieApi.get(
+          `?apiKey=${APIKey}&s=${movieText}&type=movie`
+        );
+        console.log("Response from API: ", response);
+      } catch (error) {
+        console.log("Error is: ", error);
+      }
+    };
+    fetchMovies();
+  }, []);
   return (
     <div>
-      <h1> Home </h1>
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
   );
 };
