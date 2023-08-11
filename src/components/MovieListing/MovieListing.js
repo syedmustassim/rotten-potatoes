09487625ff04d173c78bc98beb86e47e0f11662b
Syed Mustassim <smustassim@gmail.com>
2023-08-11 22:36:47 +0530

    feat: TV series displayed

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllShows } from "../../features/movies/movieSlice";
 
 import MovieCard from "../MovieCard/MovieCard";
 
@@ -8,12 +9,24 @@ import "./MovieListing.scss";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
 
-  let renderMovies = "";
+  let renderMovies,
+    renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
       movies.Search.map((item, index) => <MovieCard key={index} data={item} />)
+    ) : (
+      <div className="movies-error">
+        {" "}
+        <h3>Error: {movies.Error}</h3>
+      </div>
+    );
+
+  renderShows =
+    movies.Response === "True" ? (
+      shows?.Search?.map((item, index) => <MovieCard key={index} data={item} />)
     ) : (
       <div className="movies-error">
         {" "}
@@ -25,6 +38,10 @@ const MovieListing = () => {
       <div className="movie-list">
         <h2> Movies </h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2> Shows </h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
