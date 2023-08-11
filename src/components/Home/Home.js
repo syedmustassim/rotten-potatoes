8426c34c72056cfbbc9faf97832d178602825f8f
Syed Mustassim <smustassim@gmail.com>
2023-08-11 12:20:51 +0530

    feat: created movie Slice

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,19 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import MovieApi from "../../common/apis/MovieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "Black";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Black";
     const fetchMovies = async () => {
       try {
         const response = await MovieApi.get(
           `?apiKey=${APIKey}&s=${movieText}&type=movie`
         );
+        dispatch(addMovies(response.data));
         console.log("Response from API: ", response);
       } catch (error) {
         console.log("Error is: ", error);
